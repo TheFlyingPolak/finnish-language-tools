6da66467ff0c87b8605f51ce2964887b8a65d34b
Jakub Gajewski <jakubgajewski14@gmail.com>
2026-06-12 23:18:04 +0300

    Added random verb form generation, improved word verification against target form

--- inflection.py
+++ inflection.py
@@ -6,6 +6,11 @@ import sys
 v = Voikko("fi")
 SIJAMUOTO = "SIJAMUOTO"
 NUMBER = "NUMBER"
+TENSE = "TENSE"
+PERSON = "PERSON"
+PARTICIPLE = "PARTICIPLE"
+
+TENSE_MAP = {"present_simple": "present", "past_imperfective": "past imperfect"}
 
 SIJAMUOTO_LIST = [
     "nimento",
@@ -44,6 +49,7 @@ SIJAMUOTO_MAP = {
 }
 
 NUMBER_LIST = ["singular", "plural"]
+PERSON_LIST = ["1", "2", "3"]
 
 
 def get_random_case(word: str) -> dict[str, str] | None:
@@ -64,7 +70,7 @@ def get_random_case(word: str) -> dict[str, str] | None:
                 "SIJAMUOTO": choice(SIJAMUOTO_LIST),
             }
         case "teonsana":
-            return None
+            return get_random_verb_form(word)
         case "laatusana":
             return None
 
@@ -73,19 +79,62 @@ def get_random_case(word: str) -> dict[str, str] | None:
 
 def verify_word(word: str, target_form: dict[str, str]) -> bool:
     analysis = v.analyze(word)
-    target_sijamuoto = target_form[SIJAMUOTO]
-    target_number = target_form[NUMBER]
-    return any(
-        a[SIJAMUOTO] == target_sijamuoto and a[NUMBER] == target_number
-        for a in analysis
-    )
+    print("Word", word)
+    print("Target form", target_form)
+    return any(_verify_word_helper(a, target_form) for a in analysis)
+    # target_sijamuoto = target_form[SIJAMUOTO]
+    # target_number = target_form[NUMBER]
+    # return any(
+    #    a[SIJAMUOTO] == target_sijamuoto and a[NUMBER] == target_number
+    #    for a in analysis
+    # )
+
+
+def _verify_word_helper(analysis: dict, target_form: dict[str, str]):
+    print("Analysis", {k: analysis[k] for k in target_form.keys()})
+    for key, value in target_form.items():
+        if key not in analysis or analysis[key] != value:
+            return False
+    return True
+
+
+def get_random_verb_form(word: str) -> dict[str, str]:
+    tense = choice(["present", "imperfect", "perfect"])
+    match tense:
+        case "present":
+            return {
+                "BASEFORM": word,
+                "CLASS": "teonsana",
+                "TENSE": "present_simple",
+                "PERSON": choice(PERSON_LIST),
+                "NUMBER": choice(NUMBER_LIST),
+            }
+        case "imperfect":
+            return {
+                "BASEFORM": word,
+                "CLASS": "teonsana",
+                "TENSE": "past_imperfective",
+                "PERSON": choice(PERSON_LIST),
+                "NUMBER": choice(NUMBER_LIST),
+            }
+        # case "perfect":
+        # TODO: return target base form for the word "olla"
+        # TODO: set BASEFORM as the actual target word instead of infinitive
+        # return {"BASEFORM": word, "CLASS": "laatusana", "PARTICIPLE": "past_active"}
 
 
 def main():
     if len(sys.argv) <= 1:
-        print("Usage: inflection.py xml_file_path")
+        print("Usage: inflection.py xml_file_path [word_class]")
         return
     dictionary_path = sys.argv[1]
+
+    word_class = ""
+    if len(sys.argv) >= 2:
+        word_class = sys.argv[2]
+        if word_class != "noun" and word_class != "verb":
+            print("Parameter word_class can only have value 'noun' or 'verb'")
+            return
 
     right_count = 0
     wrong_count = 0
@@ -95,14 +144,20 @@ def main():
     )
 
     while True:
-        random_word = get_random_word(dictionary_path, "noun")
+        current_word_class = (
+            choice(["noun", "verb"]) if word_class == "" else word_class
+        )
+        print("word class", current_word_class)
+        # TODO: do not return word from dictionary which itself has no analysis result from voikko
+        random_word = get_random_word(dictionary_path, current_word_class)
         target_form = get_random_case(random_word)
         if target_form is None:
             continue
-        number = target_form[NUMBER]
-        case = SIJAMUOTO_MAP[target_form[SIJAMUOTO]]
 
-        print("Write the word", random_word, "in the", number, case, "form")
+        if current_word_class == "noun":
+            _prompt_noun(random_word, target_form)
+        elif current_word_class == "verb":
+            _prompt_verb(random_word, target_form)
         input_word = input()
 
         if input_word == "end":
@@ -118,6 +173,31 @@ def main():
         else:
             wrong_count += 1
             print("Incorrect!")
+
+
+def _prompt_noun(random_word: str, target_form: dict[str, str]):
+    number = target_form[NUMBER]
+    case = SIJAMUOTO_MAP[target_form[SIJAMUOTO]]
+    print("Write the word", random_word, "in the", number, case, "form")
+
+
+def _prompt_verb(random_word: str, target_form: dict[str, str]):
+    if TENSE in target_form:
+        tense = TENSE_MAP[target_form[TENSE]]
+        person = target_form[PERSON]
+        number = target_form[NUMBER]
+        print(
+            "Write the word",
+            random_word,
+            "in the",
+            person,
+            "person",
+            number,
+            tense,
+            "form",
+        )
+    else:
+        print("Write the word", random_word, "in the past perfect form")
 
 
 if __name__ == "__main__":
